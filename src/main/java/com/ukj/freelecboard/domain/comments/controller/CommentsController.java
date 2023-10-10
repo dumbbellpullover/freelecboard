a4445c6eb638dc0d75b9d8f274d0ded779a089ef
dumbbellpullover <dumbbellpullover@gmail.com>
2023-10-10 19:41:02 +0900

    마크다운으로 입력할 수 있는 기능 추가

--- src/main/java/com/ukj/freelecboard/domain/comments/controller/CommentsController.java
+++ src/main/java/com/ukj/freelecboard/domain/comments/controller/CommentsController.java
@@ -54,9 +54,9 @@ public class CommentsController {
 
     @GetMapping("/edit/{id}")
     public String updateForm(@PathVariable Long postId,
-                         @PathVariable Long id,
-                         Model model,
-                         Principal principal) {
+                             @PathVariable Long id,
+                             Model model,
+                             Principal principal) {
         CommentsResponseDto responseDto = commentsService.findById(id);
         if (!responseDto.getAuthorName().equals(principal.getName())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
